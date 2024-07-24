1a58df018940a98a45706329edcea26541743236
julianeschrittenlacher <julianeschrittenlacher@gmail.com>
2024-07-24 14:36:46 +0200

    add edit appointment backend tests

--- backend/src/test/java/org/example/backend/controller/AppointmentControllerTest.java
+++ backend/src/test/java/org/example/backend/controller/AppointmentControllerTest.java
@@ -71,4 +71,27 @@ class AppointmentControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.delete("/api/calender/{id}","1"))
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
+
+    @Test
+    void updateAppointment_ShouldReturnHttpOK_WhenCalledWithId() throws Exception {
+        appointmentRepository.save(new Appointment("1","test", Instant.parse("2024-07-16T09:00:00Z"),Instant.parse("2024-07-16T10:00:00Z")));
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/calender/{id}","1")
+        .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                         {
+                          "description": "test",
+                          "startTime": "2024-07-16T09:00:00Z",
+                          "endTime": "2024-07-16T10:00:00Z"
+                        }
+                    
+                        """))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.content().json("""
+                          {
+                           "description": "test",
+                           "startTime": "2024-07-16T09:00:00Z",
+                           "endTime": "2024-07-16T10:00:00Z"
+                         }
+                         """));
+    }
 }
