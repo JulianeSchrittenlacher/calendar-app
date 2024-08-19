bdfdd67fa622a6057db5c937e7b8702bbc897cf6
julianeschrittenlacher <julianeschrittenlacher@gmail.com>
2024-08-19 16:27:21 +0200

    fix exception throwing in HolidayService

--- backend/src/main/java/org/example/backend/controller/HolidayController.java
+++ backend/src/main/java/org/example/backend/controller/HolidayController.java
@@ -1,6 +1,7 @@
 package org.example.backend.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.backend.exceptions.BadRequestException;
 import org.example.backend.model.Holidays;
 import org.example.backend.service.HolidayService;
 import org.springframework.http.HttpStatus;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -21,13 +21,12 @@ public class HolidayController {
     private final HolidayService holidayService;
 
     @GetMapping("/{year}/{state}")
-    public ResponseEntity<List<Holidays>> getHolidaysByYearAndState(@PathVariable String year, @PathVariable String state) throws IOException {
+    public ResponseEntity<List<Holidays>> getHolidaysByYearAndState(@PathVariable String year, @PathVariable String state) throws BadRequestException {
         List<Holidays> holidays = holidayService.getHolidaysByYearAndState(year, state);
 
         if (holidays == null || holidays.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
-
         return ResponseEntity.ok(holidays);
     }
 }
